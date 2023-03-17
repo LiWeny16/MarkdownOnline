6c2ffca223efe71529d3df2a2c2a64f6d043ddd4
LiWeny16 <a454888395@gmail.com>
2023-03-17 15:28:59 +0800

    update

--- source/js/remember.js
+++ source/js/remember.js
@@ -1,10 +1,12 @@
 function getRememberText() {
     let text = kit.getCookie("contentText").replace(/\<\? br \?\>/g,"\n")
+    text = text.replace(/\<\? ; \?\>/g,";")
     return text
 }
 function restoreText() {
     let md = getMdText()
     md = md.replace(/\n/g, "<? br ?>")
+    md = md.replace(/;/g,"<? ; ?>")
     kit.setCookie("contentText", md, 30, "/", "md.bigonion.cn")
 }
 function fillInRemeText(){
