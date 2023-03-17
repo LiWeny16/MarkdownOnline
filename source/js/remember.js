0709d9b5f92d6600037bfdc3d3d8b923afb05942
LiWeny16 <a454888395@gmail.com>
2023-03-17 15:50:54 +0800

    update

--- source/js/remember.js
+++ source/js/remember.js
@@ -1,13 +1,14 @@
 function getRememberText() {
     let text = kit.getCookie("contentText").replace(/\<\? br \?\>/g,"\n")
-    text = text.replace(/\<\? \; \?\>/g,";")
+    text = text.replace(/\<\? semicolon \?\>/g,";")
     return text
 }
 function restoreText() {
     let md = getMdText()
     md = md.replace(/\n/g, "<? br ?>")
-    md = md.replace(/\;/g,"<? ; ?>")
+    md = md.replace(/\;/g,"<? semicolon ?>")
     kit.setCookie("contentText", md, 30, "/", "md.bigonion.cn")
+    // kit.setCookie("contentText", md, 30, "/", "127.0.0.1")
 }
 function fillInRemeText(){
     let text = getRememberText()
