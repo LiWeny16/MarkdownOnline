dc129ae12b698ab28f0816984dfd6fbd09bb0c49
LiWeny16 <a454888395@gmail.com>
2024-01-17 00:39:26 +0800

    😘update:Image renderer

--- source/js/functions/Events/aboutBox.ts
+++ source/js/functions/Events/aboutBox.ts
@@ -1,7 +1,7 @@
 import * as marked_esm from "@cdn-marked"
 import svg_1 from "@Asset/img/aboutBox关闭.svg"
 import welcomeText from "@Asset/about.md?raw"
-export default () => {
+export default async () => {
   let shutDownSvg = `
 <div class="closeBox" id="closeAbout">
 <img id="closeAboutSvg" class="closeSvg" src=${svg_1} alt="">
@@ -10,7 +10,7 @@ export default () => {
   let aboutMd = welcomeText
   if (document.getElementById("aboutMd")) {
     document.getElementById("aboutMd")!.innerHTML =
-      shutDownSvg + md2Html(aboutMd)
+      shutDownSvg + (await md2Html(aboutMd))
   }
 
   function md2Html(md: string) {
